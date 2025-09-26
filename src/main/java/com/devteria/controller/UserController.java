a79ef81c02fc6b9266c90bd19f8c85b673fd6b67
nguyenhung252006 <nguyenviethungk56@gmail.com>
2025-09-26 23:24:35 +0700

    second update

--- src/main/java/com/devteria/controller/UserController.java
+++ src/main/java/com/devteria/controller/UserController.java
@@ -1,21 +1,51 @@
 package com.devteria.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devteria.dto.request.UserCreationRequest;
+import com.devteria.dto.request.UserUpdateRequest;
 import com.devteria.identify_service.entity.User;
 import com.devteria.service.UserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping("")
     User createUser (@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
+    }
+
+    @GetMapping("")
+    List<User> getUsers () {
+        return userService.getUsers(); 
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    } 
+
+    @PutMapping ("/{userId}") 
+    User updateUser (@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser (@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "deleted user";
     }
 }
--- src/main/java/com/devteria/service/UserService.java
+++ src/main/java/com/devteria/service/UserService.java
@@ -1,9 +1,12 @@
 package com.devteria.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devteria.dto.request.UserCreationRequest;
+import com.devteria.dto.request.UserUpdateRequest;
 import com.devteria.identify_service.entity.User;
 import com.devteria.repository.UserRepository;
 
@@ -22,5 +25,25 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userReponsitory.save(user);
+    }
+
+    public List<User>getUsers() {
+        return userReponsitory.findAll();
+    }
+
+    public User getUser (String id) {
+        return userReponsitory.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    public User updateUser (String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+        return userReponsitory.save(user);
+    }
+
+    public void deleteUser (String userId) {
+        userReponsitory.deleteById(userId);
     }
 }
